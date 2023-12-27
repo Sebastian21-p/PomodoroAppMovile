4d6460551c1e09e500f964ab27d1f2a1cd125040
SebastianBer <jesussebastianbber@ufps.edu.co>
2023-12-27 11:55:08 -0500

    Primer commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  Button,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useState } from "react";
+import Header from "./src/components/Header";
+import Timer from "./src/components/Timer";
+import {Audio} from "expo-av";
+
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
 
 export default function App() {
+  const [isWorking, setWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "BREAK" | "SHORT");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() =>{
+    let interval = null;
+
+    if(isActive){
+      interval = setInterval(() => {
+        setTime(time-1);
+      }, 1000);
+    } else{
+      clearInterval(interval);
+    }
+    if(time===0){
+      setIsActive(false);
+      setWorking((prev)=>!prev);
+      setTime(isWorking?300:1500);
+    }
+    return() => clearInterval(interval);
+  },[isActive, time]);
+
+  function handleStartStop(){
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound(){
+    const {sound} = await Audio.Sound.createAsync(
+      require("./assets/jump.mp3")
+    )
+    await sound.playAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 50,
+          borderWidth: 3,
+        }}
+      >
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "center",
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
   },
 });
